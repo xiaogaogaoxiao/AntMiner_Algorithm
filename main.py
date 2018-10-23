d1878229f28f5e0f4f05d360a9aaadea6bf7d894
Juliana B Mattos <julianabarcellosmattos@gmail.com>
2018-10-23 08:51:29 -0300

    11th commit: data input adjusted

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from ant_miner import *
 def main():
 
     # INPUT: USER-DEFINED PARAMETERS:
-    NO_OF_ANTS = 30
-    MIN_CASE_PER_RULE = 2
-    MAX_UNCOVERED_CASES = 3
-    NO_RULES_CONVERG = 5
+    NO_OF_ANTS = 3000
+    MIN_CASE_PER_RULE = 10
+    MAX_UNCOVERED_CASES = 10
+    NO_RULES_CONVERG = 10
 
     # INPUT: DATASET AND CLASS ATTRIBUTE NAME
-    data = pd.read_csv('datasets/data_SSDP.csv', delimiter=';')
-    class_attr = 'Label'
+    header = list(pd.read_csv('datasets/tic-tac-toe_header.txt', delimiter=','))
+    data = pd.read_csv('datasets/tic-tac-toe_data.txt', delimiter=',', header=None, names=header, index_col=0)
+    class_attr = 'Class'
 
     # Object: DATASET
     dataset_SSDP = cDataset(data, class_attr)
