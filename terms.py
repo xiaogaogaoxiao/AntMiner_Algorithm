da6cdcfaebd3c709e8d1eb0e3b9a2498c83eec78
Juliana B Mattos <julianabarcellosmattos@gmail.com>
2019-02-05 11:25:56 -0300

    new entropy calculation method

--- terms.py
+++ terms.py
@@ -1,5 +1,6 @@
 import collections
 import math
+import numpy as np
 
 
 class cTerms:
@@ -16,49 +17,56 @@ class cTerms:
 
     def set_entropy(self, dataset):
 
-        qtd_values = len(dataset.data)
-
-        # A PRIORI PROBABILITY: P(W)
-        prob_apriori = {}
-        class_freq = dict(collections.Counter(dataset.data[:, dataset.col_index[dataset.class_attr]]))
-        for w in dataset.class_values:
-            prob_apriori[w] = class_freq[w]/qtd_values
-
-        # EVIDANCE: P(A=V)
-        freq = dict(collections.Counter(dataset.data[:, dataset.col_index[self.attribute]]))
-        prob_evidance = freq[self.value]/qtd_values
-
-        # LIKELIHOOD: P(A=V|W)
-        prob_likelihood = {}
-        for w in dataset.class_values:
-            value_freq = 0
-            for line in range(qtd_values):
-                if dataset.data[line, dataset.col_index[dataset.class_attr]] == w:
-                    if dataset.data[line, dataset.col_index[self.attribute]] == self.value:
-                        value_freq += 1
-            prob_likelihood[w] = value_freq/class_freq[w]
+        # # A PRIORI PROBABILITY: P(W)
+        # prob_apriori = {}
+        # class_freq = dict(collections.Counter(dataset.data[:, dataset.col_index[dataset.class_attr]]))
+        # for w in dataset.class_values:
+        #     prob_apriori[w] = class_freq[w]/qtd_values
+        #
+        # # EVIDANCE: P(A=V)
+        # freq = dict(collections.Counter(dataset.data[:, dataset.col_index[self.attribute]]))
+        # prob_evidance = freq[self.value]/qtd_values
+        #
+        # # LIKELIHOOD: P(A=V|W)
+        # prob_likelihood = {}
+        # for w in dataset.class_values:
+        #     value_freq = 0
+        #     for line in range(qtd_values):
+        #         if dataset.data[line, dataset.col_index[dataset.class_attr]] == w:
+        #             if dataset.data[line, dataset.col_index[self.attribute]] == self.value:
+        #                 value_freq += 1
+        #     prob_likelihood[w] = value_freq/class_freq[w]
 
         # A POSTERIORI PROBABILITY: P(W|A=V)
-        prob_posteriori = {}
-        for w in dataset.class_values:
-            prob_posteriori[w] = (prob_likelihood[w] * prob_apriori[w]) / prob_evidance
+        class_idx = dataset.col_index[dataset.class_attr]
+        attr_idx = dataset.col_index[self.attribute]
+        data = dataset.data
+        # rows = data.index[data[self.attribute] == self.value].tolist()
+        rows = list(np.where(data[:, attr_idx] == self.value)[0])
+        value_freq = len(rows)
+
+        prob_posteriori = {}.fromkeys(dataset.class_values, 0)
+        for r in rows:
+            prob_posteriori[data[r, class_idx]] += 1
 
         # ENTROPY
-        w_entropy = []
-        for w in dataset.class_values:
-            if prob_posteriori[w] == 0:
-                entropy = 0
-            else:
-                entropy = (-1) * prob_posteriori[w] * math.log2(prob_posteriori[w])
-            w_entropy.append(entropy)
-        self.entropy = sum(w_entropy)
+        if value_freq > 0:
+            entropy = 0
+            for w in prob_posteriori:
+                w_freq = prob_posteriori[w]
+                if w_freq != 0:
+                    entropy -= w_freq * math.log2(w_freq)
+            self.entropy = entropy
+        else:
+            print('Heuristic calc error: Term value doesnt appear in current dataset')
+
         self.logK_entropy = math.log2(len(dataset.class_values)) - self.entropy
 
         return
 
     def set_heuristic(self, k, denominator):
 
-        if float(denominator) == 0.0:
+        if float(denominator) == 0.0:   # CHECK THE OCCURRENCE OF THIS CONDITION <<<<<<<<
             denominator = 0.0000001
 
         log_k = math.log2(k)
